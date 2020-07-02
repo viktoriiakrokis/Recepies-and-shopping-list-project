9d91e266127b8b803abced1240da49a66ede9853
Viktoria <vika.krokis063@gmail.com>
2020-07-02 15:19:54 +0300

    build content of app

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe }  from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply test', 'https://images.immediate.co.uk/production/volatile/sites/2/2018/08/Peanut-butter-pancakes-78d1366.jpg?webp=true&quality=45&crop=9px%2C2330px%2C5697px%2C2451px&resize=1880%2C799')
+  ];
   constructor() { }
 
   ngOnInit(): void {
